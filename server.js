a3e83e16539fc74802cf0bdff457520c6b9ded1d
Nesrine Bounouh <43906541+nesrinebnh@users.noreply.github.com>
2021-12-04 22:24:55 +0100

    create server side in grpc

---
+++ server.js
@@ -0,0 +1,26 @@
+const grpc = require("grpc");
+const protoLoader = require("@grpc/proto-loader")
+const packageDef = protoLoader.loadSync("todo.proto", {});
+const grpcObject = grpc.loadPackageDefinition(packageDef);
+const todoPackage = grpcObject.todoPackage;
+
+const server  = new grpc.Server();
+server.bind("0.0.0.0:40000", 
+    grpc.ServerCredentials.createInsecure());//listen to all services
+//give our server a service
+server.addService(todoPackage.Todo.service,
+    {
+        "createTodo": createTodo,
+        "readTodos": readTodos
+
+
+    });
+server.start();
+
+function createTodo(call, callback){
+    console.log(call);
+}
+
+function readTodos(call, callback){
+    console.log(call);
+}
